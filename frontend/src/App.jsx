5a294adb719aabab5b153cf773dc3b6b4a75e25f
Gonzalez <guilherme.gonzalez@metodo12p.com.br>
2025-10-30 12:52:15 -0300

    testes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import DashConsultoria from "./pages/dashConsultoria/dashConsultoria";
 import DashLastDance from "./pages/dashLastDance/dashLastDance";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/consultoria" element={<DashConsultoria />} />
         <Route path="/lastdance" element={<DashLastDance />} />
-        <Route path="/" element={<DashConsultoria />} />
+        <Route path="/" element={<DashLastDance />} />
       </Routes>
     </HashRouter>
   );
