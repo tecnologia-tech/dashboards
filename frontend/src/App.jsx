85d91e22447d84d6ea4abcb13d9950674493b52c
Gonzalez <guilherme.gonzalez@metodo12p.com.br>
2025-10-30 12:38:03 -0300

    Troca BrowserRouter por HashRouter para garantir rotas no Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import DashConsultoria from "./pages/dashConsultoria/dashConsultoria";
 import DashLastDance from "./pages/dashLastDance/dashLastDance";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/consultoria" element={<DashConsultoria />} />
         <Route path="/lastdance" element={<DashLastDance />} />
         <Route path="/" element={<DashConsultoria />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
