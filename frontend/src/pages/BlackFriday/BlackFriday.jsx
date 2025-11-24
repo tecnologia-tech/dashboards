6093229b288c70b4f42c01151ca1242ffb39d38e
tecnologia-tech <tecnologia@metodo12p.com.br>
2025-11-24 19:53:35 -0300

    ajuste

--- frontend/src/pages/BlackFriday/BlackFriday.jsx
+++ frontend/src/pages/BlackFriday/BlackFriday.jsx
@@ -134,7 +134,7 @@ export default function BlackFriday() {
           "CONSULTORIA LANNISTER 🦁",
           "REPEDIDO 🏆",
           "GANHO PRODUTO 🧸",
-          "GANHO FRETE 🚢",
+          "BONUS PARCEIROS 🚢🧾💱",
           "FEE MENSAL 🚀",
         ];
 
